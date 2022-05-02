218ae8bf6d14c78ded29b2175970451f892e2c7d
Mark Foyster <mxfoyster@yahoo.co.uk>
2022-05-02 14:14:20 +0100

    Start to multi firing, needs tweaking but time to sync

--- js/game.js
+++ js/game.js
@@ -1,20 +1,18 @@
 document.addEventListener("keydown", function(e){KeyDownInput(e)});
 document.addEventListener("keyup", function(e){KeyUpInput(e)});
 const ship = document.getElementById("ship");
-const projectile = document.getElementById("projectile");
+const arsenal = document.getElementById("arsenal");
 
 //set up our start values
 let xPos = (screen.width/2);
-let firing = false;
-let projectileYpos = 75 + (screen.height * 0.05);
-const projectileStartY = projectileYpos;
-let projectileXpos = (screen.width / 2);
-let numOfProjectiles = 3;
+const projectileStartY = 75 + (screen.height * 0.05);
+let lastYpos = projectileStartY;
+let numOfProjectiles = 4;
+let numberCurrentlyFiring = 0;
+let projectileFiringGap = (screen.height - projectileStartY) / numOfProjectiles;
 
-//position the ship and the projectile
-projectile.style.left = parseInt(ship.style.left) + 32 + "px"; 
+//position the ship
 ship.style.left = xPos + "px"; 
-
 
 //now our object for simultaneous keys detection
 let keys = {
@@ -23,9 +21,12 @@ let keys = {
     fire: false,
   };
 
+//our projectile object
 let projectileObj = {
     missileFiring: false,
     missileYpos: projectileStartY,
+    missileXpos: (screen.width / 2) - 5,
+    missileHandle: null,
 };
 
 //set up an array of projectile objects
@@ -33,31 +34,29 @@ const projectiles = [];
 for (i = 0; i < numOfProjectiles; i++)
     projectiles.push ({...projectileObj}); //copy into array using object literals {...Object}
 
-//we can go back soon and use the above to allow a set multiple of simultaneous shots
+//set up the projectile images 
+for (i = 0; i < numOfProjectiles; i++){
+    let imageString = "<img src=\"images/projectile.png\" class=\"projectiles\" id=\"projectile" + i + "\"/>\n";
+    arsenal.innerHTML+= imageString;
+}
+//and assign handles to them into array (not very DRY but when I combine these, 
+//DOM doesn't update even though style properties DO chamge!!!)
+for (i = 0; i < numOfProjectiles; i++){
+    let projectileHandleName = "projectile" + i;
+    projectiles[i].missileHandle = document.getElementById(projectileHandleName);
+}
+    
 
 //Call our main loop every millisecond
 setInterval(MainLoop,1); 
 
 //main loop (our Update loop called in theory once every ms though in practice will be slower)
 function MainLoop(){
-    if (keys.fire) 
-        firing = true;
-
-    if (firing) {
-        projectile.style.visibility = "visible";
-        projectile.style.bottom = projectileYpos + "px";
-        projectileYpos+=3;
-        if (projectileYpos > screen.height){ //projectile reaches the top of the screen
-            firing = false;
-            projectileYpos= projectileStartY;
-            projectile.style.visibility = "hidden";
-            projectile.style.left = parseInt(ship.style.left) + 32 + "px"; //update position here else on repeat it won't move with ship
-        }
-    }
-    else{
-        projectile.style.left = parseInt(ship.style.left) + 32 + "px"; 
-    }
-
+    if (keys.fire) SetFiring(); //firing logic
+    
+    //projectile moevement logic
+    projectiles.forEach(PositionProjectiles); 
+    
     //ship movement logic
     //*** TO DO *** Implement time per iteration based movement 
     //distance to account for different speed systems
@@ -102,5 +101,59 @@ function KeyUpInput(event)
         case " ":
             keys.fire = false;
             break;
+    }
+}
+
+//called on fire pressed
+function SetFiring()
+{
+    //if this is the only currently active firing instance, 
+    //trigger the flag in our first projectile and leave
+    if (numberCurrentlyFiring == 0){
+        numberCurrentlyFiring = 1;
+        projectiles[0].missileFiring = true;
+        projectiles[0].missileHandle.style.visibility = "visible";
+        //console.log(projectiles[0].missileHandle.style.bottom);
+        return;
+    }
+
+    //otherwise we call our iteration function to check the rest
+    projectiles.forEach(FireProjectiles)
+}
+
+//loop called from our SetFiring() fuction
+function FireProjectiles(thisProjectile, index, projectiles) {
+    //if this one is firing, store the y location
+    if (thisProjectile.missileFiring == true) lastYpos = thisProjectile.missileYpos;
+    //otherwise, we have a previous y location so if gap big enough, trigger the next one and leave
+    else if (lastYpos > (thisProjectile.missileYpos + projectileFiringGap)){   
+        numberCurrentlyFiring += 1;
+        thisProjectile.missileFiring = true;
+        thisProjectile.missileHandle.style.visibility = "visible";
+        return
+    }
+}
+
+//positions the current projectiles
+function PositionProjectiles(thisProjectile, index, projectiles) {
+    //if it's not firing, update the x position as we move
+    if (thisProjectile.missileFiring == false) {
+        thisProjectile.missileXpos = parseInt(ship.style.left) + 32;
+        thisProjectile.missileHandle.style.left = thisProjectile.missileXpos + "px";;
+    }
+    //if it is AND it's not reached the top, move it upwards
+    else{
+        if (thisProjectile.missileYpos < screen.height){
+            thisProjectile.missileYpos +=1;
+            thisProjectile.missileHandle.style.bottom = thisProjectile.missileYpos + "px";
+            }
+        //otherwise, it's at the top, so reset it
+        else {
+            thisProjectile.missileFiring = false;
+            thisProjectile.missileYpos = projectileStartY;
+            thisProjectile.missileHandle.style.visibility="hidden";
+            thisProjectile.missileHandle.style.left = parseInt(ship.style.left) + 32 + "px";
+            numberCurrentlyFiring -=1;
+        }
     }
 }
